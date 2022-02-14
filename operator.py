0bdd4a851254897fb3cd594acd2b5521f9bf6762
Yonghee Kwon <hee980410@gmail.com>
2022-02-14 22:02:42 +0900

    Initial Commit

---
+++ operator.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=80, debug=True)
