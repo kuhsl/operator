cdf461355b83a06da46befcf1ee057f9a546c3aa
Yonghee Kwon <hee980410@gmail.com>
2022-03-07 02:22:50 +0900

    Edit as multi-token system

--- operator.py
+++ operator.py
@@ -10,12 +10,11 @@ cur = None
 operator_id = 'operator_id_001'
 operator_pw = 'pw_operator'
 callback_url = "http://operator.example.com/cb"
+data_source_url = "http://data-source.example.com:8080"
 
 scope_list = ['banking', 'public', 'medical']
 
-access_token = None
-access_token_expires_in = None
-
+# ERROR message
 err_msg = lambda x : '[ERROR] ' + x + '\n'
 
 def check_args(args, li):
@@ -33,14 +32,18 @@ def home():
 def sign_up():
     if not check_args(request.form, ['id', 'password']):
         return err_msg('id, password required')
-
-    new_id = request.form['id']
-    new_pw = request.form['password']
+    else:
+        new_id = request.form['id']
+        new_pw = request.form['password']
     
     sql  = "INSERT INTO user (id, pw) "
-    sql += "VALUES ('%s', '%s');"%(new_id, new_pw)
-
+    sql += "VALUES ('%s', '%s')"%(new_id, new_pw)
     cur.execute(sql)
+
+    sql  = "INSERT INTO token (id) "
+    sql += "VALUES ('%s')"%(new_id)
+    cur.execute(sql)
+
     db.commit()
 
     return 'sign up success\n'
@@ -50,9 +53,12 @@ def register():
     ### check id, pw
     if not check_args(request.form, ['id', 'password']):
         return err_msg('id, password required')
+    else:
+        _id = request.form['id']
+        _pw = request.form['password']
 
     sql  = "SELECT id FROM user WHERE "
-    sql += "id='%s' AND pw='%s';"%(request.form['id'], request.form['password'])
+    sql += "id='%s' AND pw='%s'"%(_id, _pw)
     cur.execute(sql)
     if len(cur.fetchall()) != 1:
         return 'wrong id or password\n'
@@ -60,15 +66,19 @@ def register():
     ### check scope
     if not check_args(request.args, ['scope']):
         return err_msg('scope required')
-    if request.args['scope'] not in scope_list:
+    else:
+        _scope = request.args['scope']
+
+    if _scope not in scope_list:
         return err_msg('wrong scope')
 
     ### make redirect response
-    redirect_url  = "http://data-source.example.com/authorize"
+    redirect_url  = data_source_url + "/authorize"
     redirect_url += "?response_type=code"
     redirect_url += "&scope=" + request.args['scope']
     redirect_url += "&operator_id=" + operator_id
     redirect_url += "&redirect_uri=" + callback_url
+    redirect_url += "&state=" + _id
 
     return redirect(redirect_url, code=302)
 
@@ -77,21 +87,28 @@ def callback():
     ### parse request and get grant code
     if not check_args(request.args, ['code']):
         return err_msg('code required')
-
-    grant_code = request.args['code']
+    else:
+        grant_code = request.args['code']
 
     ### make request for data source
-    url = 'http://data-source.example.com/token'
+    url = data_source_url + "/token"
     params = {'grant_type':'authorization_code',
                 'code':grant_code,
                 'redirect_uri':callback_url}
     headers = {'Authorization':'Basic ' + b64encode((operator_id+':'+operator_pw).encode()).decode(), 
                 'Content-Type':'application/x-www-form-urlencoded'}
-    re = requests.post(url, data = params, headers=headers)
+    response = requests.post(url, data = params, headers=headers)
+    # response : dict type
 
     ### parse response and get access token
-    # HTTP_response = receive_HTTP_response()
-	# access_token = get_HTTP_body(HTTP_response)
+    access_token = response['access_token']
+    expires_in = response['expires_in']
+
+    ### save token in db
+    sql  = "INSERT INTO user"
+    ### NOT FINISHED
+    ### 
+    ###
 
     return 'success\n'
 
@@ -100,13 +117,26 @@ def init_db():
     # create database operator;
     # create user operator@localhost identified by 'mysql_pw';
     # grant all on operator.* to operator@localhost;
+
+    ### connect db
     db = pymysql.connect(host='localhost', user='operator', passwd='mysql_pw', db='operator', charset='utf8')
     cur = db.cursor()
+
+    ### creat table "user"
     sql  = "CREATE TABLE IF NOT EXISTS user ("
     sql += "id varchar(20) UNIQUE NOT NULL, "
-    sql += "pw varchar(20) NOT NULL, "
-    sql += "token char(22))"
+    sql += "pw varchar(20) NOT NULL)"
     cur.execute(sql)
+
+    ### create table "token"
+    sql  = "CREATE TABLE IF NOT EXISTS token ("
+    sql += "id varchar(20) UNIQUE NOT NULL, "
+    sql += "b_token char(22), b_expire int, "
+    sql += "p_token char(22), p_expire int, "
+    sql += "m_token char(22), m_expire int)"
+    cur.execute(sql)
+
+    db.commit()
 
 if __name__ == '__main__':
     init_db()
