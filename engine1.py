9e28c08d15305fb099beb40b8d4c3ba44831b164
Seungmin Heo <55793819+nolzaheo@users.noreply.github.com>
2022-08-29 19:30:59 +0900

    Update engine1.py

--- engine1.py
+++ engine1.py
@@ -1,7 +1,7 @@
 import pymysql
 import pandas as pd
 import numpy as np
-from IPython.display import display 
+from IPython.display import display
 import warnings
 
 warnings.filterwarnings(action='ignore')
@@ -12,16 +12,16 @@ result={}
 def engine1(disease_num):
 
     ### connect db
-    #conn = pymysql.connect(host='163.152.30.239', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
-    #conn = pymysql.connect(host='localhost', user='operator', passwd='mysql_pw', db='operator', charset='utf8')
-    conn = pymysql.connect(host='localhost', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
+    #conn = pymysql.connect(host='163.152.71.223', user='localhost', passwd='mysql_pw', db='operator', charset='utf8')
+    conn = pymysql.connect(host='localhost', user='operator', passwd='mysql_pw', db='operator', charset='utf8')
+    #conn = pymysql.connect(host='localhost', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
 
     sql="SELECT a.name, a.ssn, relations, FLOOR((CHAR_LENGTH(relations)-CHAR_LENGTH(REPLACE(relations, 'c:','')))/2) AS child_cnt, disease_num FROM public_data a JOIN medical_data b ON a.ssn=b.ssn WHERE disease_num='"+disease_num+"' ORDER BY BIRTH ASC"
 
     df=pd.read_sql(sql, con=conn)
 
     #display(df)
-    
+
     cnt=0
     total=0
     for index, row in df.iterrows():
@@ -36,7 +36,7 @@ def engine1(disease_num):
         parsed=parsed[1:-1]
 
         #filter ssn
-        child_ssn_list=[]  
+        child_ssn_list=[]
         for child in parsed :
             child_ssn_list.append(child.split(':')[1])
         #print(child_ssn_list)
@@ -45,18 +45,18 @@ def engine1(disease_num):
         for ssn in child_ssn_list:
             total+=1
             cnt += check_child(ssn,conn,disease_num)
-    
+
     if(total==0) :
         #print('No data for disease(',disease_num,') : Can not proceed analysis')
         result[disease_num] = -1
-    else : 
+    else :
         #print('Analysis of disease(',disease_num,')has been finished successfully. \nTotal :',total,' cnt : ',cnt,' ',(cnt/total)*100,'%')
         result[disease_num] = (cnt/total)*100
-    
+
     conn.close()
 
     #print(result)
-    
+
     return result
 
 def check_child(ssn,conn,disease_num):
@@ -73,9 +73,9 @@ def check_child(ssn,conn,disease_num):
 def run():
     for disease_num in disease_list :
         engine1(disease_num)
-    
+
     #print(result)
     return result
 
-if __name__ == '__main__':
-    run()
+#if __name__ == '__main__':
+#    run()
