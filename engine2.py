eb759eed4d026ab761502573d84ea4a10b5e380a
Seungmin Heo <55793819+nolzaheo@users.noreply.github.com>
2022-08-31 16:01:38 +0900

    Update engine2.py

--- engine2.py
+++ engine2.py
@@ -1,34 +1,35 @@
 import pymysql
 import pandas as pd
 import numpy as np
-from IPython.display import display
+from IPython.display import display 
 import warnings
 
 warnings.filterwarnings(action='ignore')
 
 labels=['label1','label2','label3']
+result={}
 
 def engine2():
-    result={}
     ### connect db
-    #conn = pymysql.connect(host='163.152.71.223', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
-    conn = pymysql.connect(host='localhost', user='operator', passwd='mysql_pw', db='operator', charset='utf8')
-    #conn = pymysql.connect(host='localhost', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
+    conn = pymysql.connect(host='163.152.30.239', user='root', passwd='hw147258369!', db='db-server', charset='utf8')
     cur = conn.cursor()
 
     sql="SELECT balance, disease_num FROM financial_data a LEFT OUTER JOIN medical_data b ON a.ssn=b.ssn ORDER BY balance ASC"
     #sql="SELECT balance, disease_num FROM financial_data a JOIN medical_data b ON a.ssn=b.ssn WHERE disease_num IN ('I10','R81','E66') ORDER BY balance ASC"
 
     df=pd.read_sql(sql, con=conn)
-    #display(df)
+    display(df)
+
+    bins=[df['balance'].min(), 3584756, 7681620,df['balance'].max()]
 
     df['label']=pd.cut(df['balance'],
-                        bins=3,
+                        bins=bins,
                         labels=labels,
                         include_lowest=True)
 
-    #print(df)
 
+    print(df)
+    
     for label in labels:
 
         condition1=(df['label']==label)
@@ -38,15 +39,16 @@ def engine2():
         patient_cnt = len(df.loc[condition1 & condition2])
 
         if(tot_cnt == 0) :
-            #print('No Data for ('+label+') : Can not proceed analysis')
+            print('No Data for ('+label+') : Can not proceed analysis')
             result[label]=-1
             continue
-
-        ret=(patient_cnt/tot_cnt)*100
-
-        #print(ret)
+        
+        ret= round((patient_cnt/tot_cnt)*100, 2)
+        
+        print(ret)
         result[label]=ret
 
+    print(result)
     return result
 
 def run():
